0373b7513e2844021b2b0a27068f0ad491a418f9
Benjamin Pritchard <ben@bennyp.org>
2021-10-07 20:07:28 -0400

    Fix import & version check

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,8 +9,6 @@
 import datetime
 import os
 import sys
-
-sys.path.insert(0, os.path.abspath('../..'))
 import qcfractal
 
 # -- Path setup --------------------------------------------------------------
